210e96c565b8f0270c9cef6618b7b7871b7dcb22
Anil Sirpal <anilsirpal@myserver.ikoneu.com>
2016-11-30 16:18:42 +0000

    first commit

---
+++ login.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>IkonEU Heat seal transfers, badges, emblems</title>
+    <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+    <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+    
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:400,300|Raleway:300,400,900,700italic,700,300,600">
+    <link rel="stylesheet" type="text/css" href="css/jquery.bxslider.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- =======================================================
+        Theme Name: Baker
+        Theme URL: https://bootstrapmade.com/baker-free-onepage-bootstrap-theme/
+        Author: BootstrapMade.com
+        Author URL: https://bootstrapmade.com
+    ======================================================= -->
+  </head>
+  <body>
+
+    <div class="loader"></div>
+    <div id="myDiv">
+    <!--HEADER-->
+    <div class="header">
+      <div class="bg-color">
+        <header id="main-header">
+        <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#"><img src="ikonv5.png" width ="100"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+              <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="#main-header">Home</a></li>
+                <li class=""><a href="#feature">About</a></li>
+                <li class=""><a href="#service">Services</a></li>
+                <li class=""><a href="#portfolio">Products</a></li>
+                <li class=""><a href="#testimonial">Testimonial</a></li>
+                <li class=""><a href="#blog">Blog</a></li>
+                <li class=""><a href="#contact">Contact Us</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        </header>
+
+<!-- NAVIGATION BAR -->
+		
+		<section id="content_container">
+			
+			<section id="center-content">
+								<h2>Login to your account</h2>
+					
+				<form action="login.php" id="myform" method="post">
+				
+				  <fieldset>
+						 
+					<!-- Email -->
+					     <div>
+					       <label for="email" class="normal">Email</label> <input type="text" name="email" id="email" class="normal" required>
+					     </div>
+						 
+					<!-- Password -->
+					     <div>
+					       <label for="password" class="normal">Password</label> <input type="password" name="password" id="password" class="normal" required>
+					     </div>
+					     <div>
+					      
+					     </div>
+						 
+					
+					</fieldset>
+					<fieldset>
+                   <span style="padding-left:215px"> <input type="checkbox" name="remember" id="remember" class="normal" value="Y">Remember me on this computer</span><br><br> </fieldset>
+					<fieldset>
+					
+						<div class="controls">
+					<!-- Submit -->		 			    
+					      <input type="submit" id="submit" name="submit" value="Login" />
+					
+					<!-- Reset -->					     
+					       <input type="reset" id="reset" name="reset" value="Clear Form" />
+					     </div>
+				    </fieldset>
+				</form>	
+				
+				<section id="form-links">
+				
+					<a href="register.php">Register &raquo;</a> <a href="fpass.php">Password Reminder &raquo;</a>
+				
+				
+				</section>
+			
+			</section><!-- END CENTRE CONTENT -->
+			
+		</section><!--END CONTENT CONTAINER -->
+		
+		<footer>
+		
+	<section id="left-footer-content">	
+		<img src="images/footer-logo.jpg" alt="Ikon EU logo"><br>
+		IKON EU<br>
+		Unit 5<br>
+		Park Rose Industrial Estate<br>
+		Middlemore Road<br>
+		Smethwick<br>
+		West Midlands<br>
+		B66 2DZ<br>
+		United Kingdom<br><br>				
+		
+		Telephone: +44 (0) 121 555 7043<br>
+		Facsimile: +44 (0) 121 555 7046<br>
+		Email: info@ikoneu.com
+	</section>
+	
+	<section id="center-footer-content">
+		<h1>Quick Links</h1>
+	
+		<a href="contact_us.php">Online Enquiry &raquo;</a>
+		<a href="login.php">Login &raquo;</a>
+		<a href="mailto:info@ikoneu.com">Email Us &raquo;</a>
+		<a href="contact_us.php">Contact Page &raquo;</a>
+		<a href="terms.php">Terms and Conditions &raquo;</a>
+		<a href="privacy.php">Privacy Policy &raquo;</a>		
+	</section>
+	
+	<section id="center-footer-content_2">
+		<h1>Services and Products</h1>
+	
+		<a href="category.php">Products &raquo;</a>
+		<a href="services.php">Services &raquo;</a>
+		<a href="login.php">Design Room &raquo;</a>		
+	</section>
+	
+	<section id="right-footer-content">
+		<a href=""><img src="images/twitter.jpg" alt="Twitter link"></a>
+		<a href=""><img src="images/facebook.jpg" alt="Facebook link"></a>
+		
+		<p>&copy; Ikoneu 2016</p>
+		
+		<a href="http://www.wilson-web-design.co.uk">Website design by Wilson Web Design</a>
+	</section>
+	
+</footer>
+		
+	</div>
+	
+	
+</body>
+
+</html>
